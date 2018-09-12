49cf7969780f0b247b6a7be706a787bd86835884
Aldeo <zijiezhang@163.com>
2018-09-12 23:11:14 +0800

    增加方法

---
+++ src/main/java/com/zijiez/spring/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.zijiez.spring.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Aldeo on 2018/9/12.
+ */
+//Dao注解
+@Repository
+public class UserDao {
+}
--- src/main/java/com/zijiez/spring/service/UserService.java
+++ src/main/java/com/zijiez/spring/service/UserService.java
@@ -1,17 +1,19 @@
 package com.zijiez.spring.service;
 
 import com.zijiez.spring.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    UserDto userDto;
 
     /**
      * 查询用户信息
      * @return
      */
     public UserDto getUserInfo(){
-        UserDto userDto = new UserDto();
         userDto.setUserName("Aldeo");
         userDto.setAge(18);
         return userDto;
